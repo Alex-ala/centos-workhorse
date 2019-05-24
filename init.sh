6379947a9352182f31ed967ef6c84729c0f5e874
Alexander Landmesser <alexander@land-server.de>
2019-05-24 19:05:26 +0200

    init

--- init.sh
+++ init.sh
@@ -6,8 +6,12 @@ if [[ $(/bin/id -u) -ne 0 ]]; then
     exit 1
 fi
 
+ssh-keygen -f /home/${SUDO_USER}/.ssh/id_rsa
+cp /home/${SUDO_USER}/.ssh/id_rsa.pub /home/${SUDO_USER}/.ssh/authorized_keys
+chown -R ${SUDO_USER}:${SUDO_USER} /home/${SUDO_USER}/.ssh/
+chmod 600 /home/${SUDO_USER}/.ssh/authorized_keys
 yum update -y
-yum install ansible yum-utils
+yum -y install ansible yum-utils
 package-cleanup --oldkernels --count=2
 echo "$SUDO_USER     ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${SUDO_USER}
 echo "sh $DIR/init_after_reboot.sh $DIR" >> /home/${SUDO_USER}/.bashrc
