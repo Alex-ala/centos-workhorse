3634726d17459d27476d3d49d82f5d2b5063482e
Alexander Landmesser <alexander.landmesser@competec.ch>
2019-06-11 14:03:31 +0200

    pewpew

--- i3wm/files/i3/network.sh
+++ i3wm/files/i3/network.sh
@@ -4,7 +4,7 @@ WIFI_ADAPTER=$(iwgetid | awk '{print $1}')
 WIFI_IP=$(ip a show dev ${WIFI_ADAPTER} | grep "inet "| awk '{print $2}')
 
 declare -A LAN_IP
-LAN_ADAPTERS=$(ip link show | grep "state UP" | grep -viE "(docker|veth|lo|${WIFI_ADAPTER})" | awk '{gsub(":",""); print $2}')
+LAN_ADAPTERS=$(ip link show | grep "state UP" | grep -vE "(docker|veth|lo|${WIFI_ADAPTER})" | awk '{gsub(":",""); print $2}')
 for device in ${LAN_ADAPTERS} ; do
     LAN_IP["${device}"]=$(ip a show dev ${device} | grep "inet "| awk '{print $2}')
 done
@@ -12,7 +12,7 @@ echo "WLAN SSID:~$WIFI_SSID"
 echo "WLAN IP:~$WIFI_IP"
 echo "WLAN ADAPTER:~$WIFI_ADAPTER"
 for device in ${LAN_ADAPTERS} ; do
-    echo "LAN IP:~ "$LAN_ADAPTERS[${device}]
+    echo "LAN IP:~${LAN_IP[$device]}"
     echo "LAN ADAPTER:~ ${device}"
 done
 
