878762269c8b73b680a9ab07a67e442f91b7ff02
Alexander Landmesser <alexander.landmesser@competec.ch>
2019-06-12 11:17:36 +0200

    fixes fixes fixes

--- i3wm/files/i3/network.sh
+++ i3wm/files/i3/network.sh
@@ -4,15 +4,20 @@ WIFI_ADAPTER=$(iwgetid | awk '{print $1}')
 WIFI_IP=$(ip a show dev ${WIFI_ADAPTER} | grep "inet "| awk '{print $2}')
 
 declare -A LAN_IP
-LAN_ADAPTERS=$(ip link show | grep "state UP" | grep -vE "(docker|veth|lo|${WIFI_ADAPTER})" | awk '{gsub(":",""); print $2}')
+if [[ ! -z "$WIFI_ADAPTER" ]]; then
+	LAN_ADAPTERS=$(ip link show | grep "state UP" | grep -vE "(docker|veth|lo|${WIFI_ADAPTER})" | awk '{gsub(":",""); print $2}')
+else
+	LAN_ADAPTERS=$(ip link show | grep "state UP" | grep -vE "(docker|veth|lo)" | awk '{gsub(":",""); print $2}')
+
+fi
 for device in ${LAN_ADAPTERS} ; do
     LAN_IP["${device}"]=$(ip a show dev ${device} | grep "inet "| awk '{print $2}')
 done
-echo "WLAN SSID:~$WIFI_SSID"
-echo "WLAN IP:~$WIFI_IP"
-echo "WLAN ADAPTER:~$WIFI_ADAPTER"
+echo "WLAN SSID:~ $WIFI_SSID"
+echo "WLAN IP:~ $WIFI_IP"
+echo "WLAN ADAPTER:~ $WIFI_ADAPTER"
 for device in ${LAN_ADAPTERS} ; do
-    echo "LAN IP:~${LAN_IP[$device]}"
+    echo "LAN IP:~ ${LAN_IP[$device]}"
     echo "LAN ADAPTER:~ ${device}"
 done
 
