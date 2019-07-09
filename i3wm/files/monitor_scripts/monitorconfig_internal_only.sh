cb8344686c57577c8fe0754fce755bb772bf6a5e
Alexander Landmesser <alexander.landmesser@competec.ch>
2019-07-09 15:52:28 +0200

    pewpew xdisplay

--- i3wm/files/monitor_scripts/monitorconfig_internal_only.sh
+++ i3wm/files/monitor_scripts/monitorconfig_internal_only.sh
@@ -9,6 +9,17 @@ xrandr \
 	--output DP-2-1 --off \
 	--output DP-2-2 --off \
 	--output DP-2-3 --off \
+	--output eDP-1 --off \
+	--output HDMI-2 --off \
+	--output HDMI-1 --off \
+	--output DP-2 --off \
+	--output DP-1 --off
+
+xrandr \
+	--dpi 125 \
+	--output DP-2-1 --off \
+	--output DP-2-2 --off \
+	--output DP-2-3 --off \
 	--output eDP-1 --mode 2560x1440 --pos 0x0 --rotate normal --scale 1x1 --primary \
 	--output HDMI-2 --off \
 	--output HDMI-1 --off \
---
+++ i3wm/files/monitor_scripts/monitorconfig_internal_with_hdmi_hd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+DPI=$(xdpyinfo | grep resolution | grep -oE "[0-9]+x[0-9]+")
+if [[ $DPI != "72x72" ]]; then
+	sed -i "s/Xft.dpi:.*/Xft.dpi: 72/" ~/.Xresources
+	xrdb -merge ~/.Xresources
+fi
+xrandr \
+	--dpi 72 \
+	--output DP-2-1 --off \
+	--output DP-2-2 --off \
+	--output DP-2-3 --off \
+	--output eDP-1 --mode 2560x1440 --pos 0x0 --rotate normal --scale 0.64x0.64 --primary \
+	--output HDMI-2 --off \
+	--output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate normal \
+	--output DP-2 --off \
+	--output DP-1 --off
+i3-msg restart
+killall conky || true
+sleep 5
+conky -d -c ~/.i3/conky_scaled.conf
